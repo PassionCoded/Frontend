f883d417231a1f0bc643e6e71365e5d7945f25ef
Rick Tesmond <tesmonrd@gmail.com>
2016-07-14 18:10:32 -0700

    basic form and styling created, deal with XML error later

--- registration.js
+++ registration.js
@@ -5,23 +5,26 @@ $(document).ready(function() {
         var password = $("#password").val();
         var cpassword = $("#cpassword").val();
         if (name == '' || email == '' || password == '' || cpassword == '') {
-        alert("Please fill all fields...!!!!!!");
-        } else if ((password.length) < 8) {
-        alert("Password should atleast 8 character in length...!!!!!!");
-        } else if (!(password).match(cpassword)) {
-        alert("Your passwords don't match. Try again?");
-        } else {
-        $.post("test.json",
-            {
-                name1: name,
-                email1: email,
-                password1: password
-            }, function(data) {
-            if (data == 'You have Successfully Registered.....') {
-            $("form")[0].reset();
-            }
-            alert(data);
-            });
+            alert("Please fill all fields...!!!!!!");
+        }
+        else if ((password.length) < 8) {
+            alert("Password should atleast 8 character in length...!!!!!!");
+        }
+        else if (!(password).match(cpassword)) {
+            alert("Your passwords don't match. Try again?");
+        }
+        else {
+            $.post("test.json",
+                {
+                    name1: name,
+                    email1: email,
+                    password1: password
+                }, function(data) {
+                if (data == 'You have Successfully Registered.....') {
+                $("form")[0].reset();
+                }
+                alert(data);
+                });
         }
     });
 });
