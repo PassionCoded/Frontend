24dd27a325d74d8ad93ecc0ed28ae27423222988
Rick Tesmond <tesmonrd@gmail.com>
2016-07-16 11:24:29 -0700

    changed ajax call

--- js/registration.js
+++ js/registration.js
@@ -11,17 +11,31 @@ $(document).ready(function() {
         } else if (!(password).match(cpassword)) {
         alert("Your passwords don't match. Try again?");
         } else {
-        $.post("test.json",
-            {
-                name1: name,
-                email1: email,
-                password1: password
-            }, function(data) {
-            if (data == 'You have Successfully Registered.....') {
-            $("form")[0].reset();
-            }
-            alert(data);
+            $.ajax({
+                url: 'http://projectmypassion-api.herokuapp.com/reg_user',
+                dataType: 'json',
+                type: 'post',
+                contentType: 'application/json',
+                data: JSON.stringify( { "user": {"email": $('#email').val(), "password_confirmation": $('#cpassword').val() }} ),
+                processData: false,
+                success: function( data, textStatus, jQxhr ){
+                    $('#response pre').html( JSON.stringify( data ) );
+                },
+                error: function( jqXhr, textStatus, errorThrown ){
+                    console.log( errorThrown );
+                }
             });
+        // $.post("http://projectmypassion-api.herokuapp.com/reg_user",
+        //     {
+        //         name1: name,
+        //         email1: email,
+        //         password1: password
+        //     }, function(data) {
+        //     if (data == 'You have Successfully Registered.....') {
+        //     $("form")[0].reset();
+        //     }
+        //     alert(data);
+        //     });
         }
     });
 });
