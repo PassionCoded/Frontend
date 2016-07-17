d4fd99ab7c6a1275a13f495bf7f750c43853cf22
Rick Tesmond <tesmonrd@gmail.com>
2016-07-16 23:46:19 -0700

    cleaned up code

--- js/registration.js
+++ js/registration.js
@@ -7,10 +7,6 @@ $(document).ready(function() {
                 "password_confirmation": $("#cpassword").val()
             }
         };
-        var name = $("#name").val();
-        var email = $("#email").val();
-        var password = $("#password").val();
-        var cpassword = $("#cpassword").val();
         if (name == '' || email == '' || password == '' || cpassword == '') {
         alert("Please fill all fields...!!!!!!");
         } else if ((password.length) < 8) {
